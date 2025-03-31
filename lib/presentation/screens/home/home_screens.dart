4883f389c88fcf7c6f2c6a78d2f0e909087a640c
Juan Camilo G. <juancamilo-ortega@hotmail.com>
2025-03-31 11:38:08 -0500

    feat: implement HomeScreen

--- lib/presentation/screens/home/home_screens.dart
+++ lib/presentation/screens/home/home_screens.dart
@@ -1,4 +1,8 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/config/themes/app_theme.dart';
 
 class HomeScreens extends StatelessWidget {
   const HomeScreens({super.key});
@@ -9,6 +13,44 @@ class HomeScreens extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Fultter + Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItems = appMenuItems[index];
+
+        return  CustomListTitle(menuItems: menuItems);
+      },
+    );
+  }
+}
+
+class CustomListTitle extends StatelessWidget {
+  const   CustomListTitle({super.key, 
+  
+    required this.menuItems,
+  });
+
+  final MenuItems menuItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(menuItems.icon , color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      title: Text(menuItems.title),
+      subtitle: Text(menuItems.subTitle),
+      
     );
   }
 }
