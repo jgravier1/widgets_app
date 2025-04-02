b3187bc8c8a32c7b2134a3e028072d1f7e4ddddf
Juan Camilo G. <juancamilo-ortega@hotmail.com>
2025-04-02 11:33:05 -0500

    Fix: Resuelto conflicto en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:widgets_app/config/router/app_router.dart';
-=======
 import 'package:widgets_app/config/routes/app_routes.dart';
->>>>>>> a3dc117 ( start: beginning development of Buttons screen)
 import 'package:widgets_app/config/themes/app_theme.dart';
 
 void main() {
@@ -16,11 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp.router(
-<<<<<<< HEAD
-      routerConfig: appRouter,
-=======
       routerConfig: router,
->>>>>>> a3dc117 ( start: beginning development of Buttons screen)
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectColor: 0).getTheme(),
       
